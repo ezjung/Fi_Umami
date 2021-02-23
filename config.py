ca00bc6f10ab9088c4ed2c7c6e748562d7691253
Sungsoon Park <sungsoon.park@gmail.com>
2021-02-23 13:01:06 -0800

    2021-02-23 First git commit

---
+++ config.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+yr, mo, biMonth = "2021", "02", "B"
+
+path = '/media/sf_shared/my_program/Financial'
+
+# path_shared is the path for shared/Umami folder for a copy
+path_shared = '/media/sf_shared/Umami/Financials'
+
+period = yr + "_" + mo
+
+data = 'Data'
+
+meta = 'meta'
+report = 'report'
+
+caviar = 'caviar'
+uber = 'uber'
+shift = 'shift'
+square ='square'
+
+year, month = int(yr), int(mo)
+
+locs = ["Dimond", "Uptown"]
+dn = ['Day', 'Night']
